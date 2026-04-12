a61b93ed2fd8bd04d0bf396b25b2305f1573a10e
Govani13 <137343105+Govani13@users.noreply.github.com>
2026-04-12 12:06:43 -0300

    implement Day and Time system
    - DayTimeManager: day advances via SpendTime(), randomized hours per day, fires global events (OnDayEnded, OnNewDayStarted, OnGameEnded)
    - ActionData_SO: added TimeCost field

--- Assets/Scripts/ActionData_SO.cs
+++ Assets/Scripts/ActionData_SO.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Action/Data")]
 public class ActionData_SO : ScriptableObject
 {
-    
+    [SerializeField, Min(0), Tooltip("Horas que cuesta esta acción al ejecutarse.")]
+    private int timeCost;
+
+    public int TimeCost => timeCost;
 }
--- Assets/Scripts/Global Events/GlobalEventSO_Bool.cs
+++ Assets/Scripts/Global Events/GlobalEventSO_Bool.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine;
 
 [CreateAssetMenu(menuName = "Events/Bool")]
 public class GlobalEventSO_Bool : GlobalEventSO<bool> { }
