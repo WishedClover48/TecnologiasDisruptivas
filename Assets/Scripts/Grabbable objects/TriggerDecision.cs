06ca08dbebc8759e1b359d4a43cca23d8209a2c7
WishedClover48 <matuleon2001@gmail.com>
2026-04-12 16:26:44 -0300

    Added Interactivity to objects

---
+++ Assets/Scripts/Grabbable objects/TriggerDecision.cs
@@ -0,0 +1,86 @@
+using Oculus.Interaction;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerDecision : MonoBehaviour
+{
+    [SerializeField] private GrabInteractable _grabInteractable;
+
+    [SerializeField] private Canvas _objectCanvas;
+    [SerializeField] private Image _progress;
+    [SerializeField] private float _fillDuration = 2f;
+
+    private Coroutine _fillCoroutine;
+
+    private bool _done = false;
+    public bool Done { get { return _done; } }
+
+    private void Start()
+    {
+        _grabInteractable.WhenStateChanged += ObjectInteraction;
+        _objectCanvas.gameObject.SetActive(false);
+        _done = false;
+    }
+
+    private void OnDestroy()
+    {
+        if (_grabInteractable != null)
+            _grabInteractable.WhenStateChanged -= ObjectInteraction;
+    }
+
+    private void ObjectInteraction(InteractableStateChangeArgs args)
+    {
+        if (args.NewState == InteractableState.Select)
+        {
+            _objectCanvas.gameObject.SetActive(true);
+
+            // Start filling
+            if (_progress != null)
+            {
+                _progress.fillAmount = 0f;
+
+                if (_fillCoroutine != null)
+                    StopCoroutine(_fillCoroutine);
+
+                _fillCoroutine = StartCoroutine(FillProgress());
+                _done = false;
+            }
+        }
+        else
+        {
+            _objectCanvas.gameObject.SetActive(false);
+
+            // Stop and reset filling when released
+            if (_fillCoroutine != null)
+            {
+                StopCoroutine(_fillCoroutine);
+                _fillCoroutine = null;
+            }
+
+            if (_progress != null)
+                _progress.fillAmount = 0f;
+        }
+    }
+
+    private IEnumerator FillProgress()
+    {
+        if (_fillDuration <= 0f)
+        {
+            _progress.fillAmount = 1f;
+            _done = true;
+            yield break;
+        }
+
+        float elapsed = 0f;
+        while (elapsed < _fillDuration)
+        {
+            elapsed += Time.deltaTime;
+            _progress.fillAmount = Mathf.Clamp01(elapsed / _fillDuration);
+            yield return null;
+        }
+
+        _progress.fillAmount = 1f;
+        _fillCoroutine = null;
+    }
+}
