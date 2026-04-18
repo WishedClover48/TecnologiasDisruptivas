4465323e0ae826e254d9fd7ce92a1279d7397531
WishedClover48 <matuleon2001@gmail.com>
2026-04-18 15:56:27 -0300

    Added logic to the Hover Canvas

---
+++ Assets/Scripts/Grabbable objects/ShowOnCanvas.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class ShowOnCanvas : MonoBehaviour
+{
+    [SerializeField] private Activity _activity;
+    [SerializeField] private TMP_Text _money;
+    [SerializeField] private TMP_Text _hours;
+    [SerializeField] private TMP_Text _health;
+    [SerializeField] private TMP_Text _stress;
+
+    private ActionData_SO _actionData;
+    private void Awake()
+    {
+        _actionData = _activity.GetActivity();
+
+        _money.text = TranslateActionDatatoString(_actionData.MoneyCost);
+        _hours.text = TranslateActionDatatoString(_actionData.TimeCost);
+        _stress.text = TranslateActionDatatoString(_actionData.Stress);
+        _health.text = TranslateActionDatatoString(_actionData.Health);
+
+    }
+private string TranslateActionDatatoString(int data)
+    {
+        int value = data;
+        string sign = value > 0 ? "+" : "";
+
+        return sign + value.ToString();
+    }
+}
